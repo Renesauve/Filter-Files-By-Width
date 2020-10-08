155311bb1e722e88aebf83908d3f2323320b8080
Rene Sauve <rdasauve@gmail.com>
2020-10-08 16:53:06 -0700

    wtf man

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 const fs = require("fs");
-const {parse} = require("json2csv");
-
-// const path = require("path");
-// const process = require("process");
-
-// const glob = require("glob");
-// Loop through all the files in the temp directory
+// const {parse} = require("json2csv");
+// const json2xls = require('json2xls');
 const cheerio = require("cheerio");
 
-const folderPath = "C:/Users/rdasa/Desktop/coins/";
+const { json2excel, excel2json } = require('js2excel');
 
+// const DxfParser = require("dxf-parser");
+// const pcheerio = require('pseudo-cheerio');
+
+
+const folderPath = "/Users/Student2/OneDrive - Electron Metalworks Ltd/Desktop/svgs/SVGS";
 fs.readdir(folderPath, {encoding: "utf8", withFileTypes: true}, function (
   err,
   files
 ) {
   Promise.all(
     files.map(async (file, index) => {
+
       if (file.isFile()) {
         return new Promise((resolve, reject) => {
+       
           function readContent(callback) {
             fs.readFile(
               `${folderPath}` + "/" + `${file.name}`,
@@ -25,33 +27,34 @@ fs.readdir(folderPath, {encoding: "utf8", withFileTypes: true}, function (
               function (err, content) {
                 if (err) reject(err);
                 callback(null, content);
+             
               }
             );
           }
-
+        
           readContent(function (err, content) {
             const $ = cheerio.load(content);
-            // console.log(file);
-
-            const svg = $(`svg`);
+            
+            const svg = $("svg");
             const fileData = {
               filename: file.name,
               width: svg.attr("width"),
               height: svg.attr("height"),
-              docname: svg.attr("sodipodi:docname"),
             };
             resolve(fileData);
-
-            // allFilesData.push(fileData);
           });
         });
       }
     })
-  ).then((allFilesData) => {
-    const fields = ["field1", "field2", "field3", "field4"];
-    const csv = parse(allFilesData, {fields});
-
-    console.log(csv);
-  });
+  ).then((content) => 
+      json2excel({
+        content,
+        name: content.filename,
+        width: content.width,
+        height: content.height   
+    }
+  ).catch(
+    console.log("okay m8")
+  ))
 });
-// // console.log(allFilesData);
+
