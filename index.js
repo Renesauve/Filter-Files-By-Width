4673b09f9265b933626b3fe6118b8441e5520687
Rene <rdasauve@gmail.com>
2020-10-08 22:48:58 -0700

    Json-To-CSV
    added jsonexport..
    
    json2xls and json2excel don't seem to be working anymore.

--- index.js
+++ index.js
@@ -2,24 +2,22 @@ const fs = require("fs");
 // const {parse} = require("json2csv");
 // const json2xls = require('json2xls');
 const cheerio = require("cheerio");
+const jsonexport = require("jsonexport");
 
-const { json2excel, excel2json } = require('js2excel');
+// const {json2excel, excel2json} = require("js2excel");
 
 // const DxfParser = require("dxf-parser");
 // const pcheerio = require('pseudo-cheerio');
 
-
-const folderPath = "/Users/Student2/OneDrive - Electron Metalworks Ltd/Desktop/svgs/SVGS";
+const folderPath = "/mnt/c/Users/rdasa/Desktop/coins";
 fs.readdir(folderPath, {encoding: "utf8", withFileTypes: true}, function (
   err,
   files
 ) {
   Promise.all(
     files.map(async (file, index) => {
-
       if (file.isFile()) {
         return new Promise((resolve, reject) => {
-       
           function readContent(callback) {
             fs.readFile(
               `${folderPath}` + "/" + `${file.name}`,
@@ -27,34 +25,32 @@ fs.readdir(folderPath, {encoding: "utf8", withFileTypes: true}, function (
               function (err, content) {
                 if (err) reject(err);
                 callback(null, content);
-             
               }
             );
           }
-        
+
           readContent(function (err, content) {
             const $ = cheerio.load(content);
-            
             const svg = $("svg");
             const fileData = {
               filename: file.name,
               width: svg.attr("width"),
               height: svg.attr("height"),
             };
+
             resolve(fileData);
           });
-        });
+        }).catch((error) => console.log({error}));
       }
     })
-  ).then((content) => 
-      json2excel({
-        content,
-        name: content.filename,
-        width: content.width,
-        height: content.height   
-    }
-  ).catch(
-    console.log("okay m8")
-  ))
-});
+  )
+    .then((content) => {
+      jsonexport(content, function (err, csv) {
+        if (err) return console.error(err);
+        const writer = fs.createWriteStream("out.csv");
 
+        writer.write(csv);
+      });
+    })
+    .catch((error) => console.log({error}));
+});
