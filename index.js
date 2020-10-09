5df0370448d98f8c6a64c263d28a96e9e67b0dd0
Rene Sauve <rdasauve@gmail.com>
2020-10-09 14:55:38 -0700

    Filters-images-by-width

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ const fs = require("fs");
 const cheerio = require("cheerio");
 const jsonexport = require("jsonexport");
 
+// const path = require("path");
 // const {json2excel, excel2json} = require("js2excel");
 
 // const DxfParser = require("dxf-parser");
 // const pcheerio = require('pseudo-cheerio');
 
-const folderPath = "/mnt/c/Users/rdasa/Desktop/coins";
-fs.readdir(folderPath, {encoding: "utf8", withFileTypes: true}, function (
+const currentPath = "C:/Users/Student2/OneDrive - Electron Metalworks Ltd/Desktop/svgs/SVGS";
+
+
+
+fs.readdir(currentPath, {encoding: "utf8", withFileTypes: true}, function (
   err,
   files
 ) {
@@ -20,37 +24,75 @@ fs.readdir(folderPath, {encoding: "utf8", withFileTypes: true}, function (
         return new Promise((resolve, reject) => {
           function readContent(callback) {
             fs.readFile(
-              `${folderPath}` + "/" + `${file.name}`,
+              `${currentPath}` + "/" + `${file.name}`,
               "utf8",
               function (err, content) {
                 if (err) reject(err);
                 callback(null, content);
+      
               }
             );
           }
+     
+
 
           readContent(function (err, content) {
+
             const $ = cheerio.load(content);
             const svg = $("svg");
-            const fileData = {
+            const fileData = 
+              {
               filename: file.name,
               width: svg.attr("width"),
               height: svg.attr("height"),
-            };
+            }
+            
+             resolve(([fileData].filter(item => item.width != "210mm")))
+            
 
-            resolve(fileData);
           });
+        
+
         }).catch((error) => console.log({error}));
       }
+ 
+
     })
-  )
-    .then((content) => {
+    
+  ).then((content) => {
+
+for ( i=0; i<content.length; i++ )
+
+          {
+        for ( j=0; j<content[i].length; j++ )
+      
+        {
+          const oldPath = (currentPath + "/" + content[i][j].filename)
+          const newPath = ("C:/Users/Student2/OneDrive - Electron Metalworks Ltd/Desktop/svgs/NeedsFixedSvgs" + "/" + content[i][j].filename)
+        
+
+          fs.rename(oldPath, newPath, function (err) {
+            if (err) throw err
+            console.log('Successfully renamed - AKA moved!')
+          })
+
+        }
+      }
+    
+    
+
+
+
       jsonexport(content, function (err, csv) {
         if (err) return console.error(err);
-        const writer = fs.createWriteStream("out.csv");
+        const writer = fs.createWriteStream("filtered-pictures.csv");
 
         writer.write(csv);
       });
-    })
-    .catch((error) => console.log({error}));
+
+    }).catch((error) => console.log({error}));
 });
+
+
+
+
